451585da49a6ce3855025fa47b21c8ff36500413
try496 <66765024+try496@users.noreply.github.com>
2022-11-22 08:48:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,22 +11,11 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
+# sed -i 's/\"#src-git\"/\"src-git\"/g' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-## 常用OpenWrt软件包源码合集，同步上游更新！
-## 通用版luci适合18.06与19.07
-# echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
-
-
-## 解除系统限制
-ulimit -u 10000
-ulimit -n 4096
-ulimit -d unlimited
-ulimit -m unlimited
-ulimit -s unlimited
-ulimit -t unlimited
-ulimit -v unlimited
+# other
+# rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
